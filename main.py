a89fedca4056629fc82930d36c568ea332f2eb32
TMGilchrist <telcontar@hotmail.co.uk>
2017-11-12 22:05:52 +0000

    Added best sound ever Added create_sine function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy
+import math
 
 
 class App:
@@ -10,8 +12,11 @@ class App:
 
     def run(self):
         pygame.init()
+        pygame.mixer.init()
 
         self.screen = pygame.display.set_mode((640, 481))  # 481 for extra uniqueness points
+
+        self.create_sine()
 
         self.running = True
         while self.running:
@@ -26,5 +31,17 @@ class App:
     def quit(self):
         running = False
 
-# Main code run!
+    def create_sine(self):
+        base_sound = pygame.mixer.Sound("wilhelmScream.wav")
+        samples = pygame.sndarray.samples(base_sound)
+
+        for index, sample in enumerate(samples):
+            sample = math.sin(2.0 * math.pi * 440 * index / 22050)
+
+        del (samples)
+        base_sound.play()
+
+        # Main code run!
+
+
 App()
