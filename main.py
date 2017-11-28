d5da2a6f064f65f2a9702343fe96913038143838
TMGilchrist <telcontar@hotmail.co.uk>
2017-11-28 21:46:46 +0000

    Replaced pygame window with Tkinter window. Added init_ui function to setup buttons. Added button to play sound.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import math
 
 import pygame
 import numpy
+import Tkinter
+import tkMessageBox
 
 from dynsound import DynSound
 
@@ -25,14 +27,32 @@ class App:
     CHANNELS = 2
     BUFFER = 4096
 
-    screen = None
+    main_screen = None
     running = False
+    edit_sound = None
 
     def __init__(self):
         """Class constructor"""
 
         # Just call run and be done!
         self.run()
+
+    def helloCallBack(self):
+        tkMessageBox.showinfo("Hello Python", "Hello World")
+
+
+
+    def init_ui(self):
+        """Initialise ui elements."""
+
+        play_preview = Tkinter.Button(self.main_screen, text="Play Sound", command=lambda: pygame.mixer.Sound.play(self.edit_sound.sound))
+
+        volume_up = Tkinter.Button(self.main_screen, text="Volume Up", command=self.edit_sound.change_volume(10))
+        volume_down = Tkinter.Button(self.main_screen, text="Volume Down", command=self.edit_sound.change_volume(-10))
+
+        play_preview.pack()
+        volume_up.pack()
+        volume_down.pack()
 
     def run(self):
         """Runs the application."""
@@ -41,32 +61,27 @@ class App:
         pygame.init()
         pygame.mixer.init(frequency=self.FREQUENCY, size=self.SIZE, channels=self.CHANNELS, buffer=self.BUFFER)
 
-        self.screen = pygame.display.set_mode((640, 481))  # 481 for extra uniqueness points
+        # self.screen = pygame.display.set_mode((640, 481))  # 481 for extra uniqueness points
+
+        self.edit_sound = self.create_sine(440, 1.0)
+
+        self.main_screen = Tkinter.Tk()
+        self.init_ui()
+        self.main_screen.mainloop()
 
         # Generate a test sound with effects
-        sound = self.create_sine(440, 1.0)
-        sound.change_frequency(0.5)
-        sound.change_volume(-0)
+        # self.edit_sound.play()
+        self.edit_sound.change_frequency(0.5)
+        self.edit_sound.change_volume(-0)
 
         wilhelm = DynSound("wilhelmscream.wav")
-        sound.mix(wilhelm)
-        sound.change_volume(-10)
-        sound.add_echo(0.3, -4, 10)
+        self.edit_sound.mix(wilhelm)
+        self.edit_sound.change_volume(-10)
+        self.edit_sound.add_echo(0.3, -4, 10)
 
-        sound.save("testSound.wav")
+        self.edit_sound.save("testSound.wav")
 
-        sound.play()
 
-        # Run the main loop
-        self.running = True
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-            self.screen.fill((255, 255, 255))
-
-            pygame.display.flip()
 
     def quit(self):
         """Quits the application"""
@@ -86,7 +101,7 @@ class App:
         samples = pygame.sndarray.samples(sound.sound)
 
         for index, sample in numpy.ndenumerate(samples):
-            samples[index[0], index[1]] = math.sin(2.0 * math.pi * frequency * index[0] / 22050)
+            samples[index[0], index[1]] = math.sin(2.0 * math.pi * frequency * index[0] / 22050) * 32767
 
         del samples
 
