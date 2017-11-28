0d26e748a22c2f5db3ef5c81cdd89020fb51c9d8
Louis <louis_foy@yahoo.co.uk>
2017-11-28 22:22:52 +0000

    Non-destructive sound manipulation with buttons!! - Added variables into the App class to support non-destructive sound manipulation - Added validate_sound to App to regenerate the sound before playing it - Adapted buttons to play sounds and change volume - Added save_sound to App - Added play_sound to App - Cleaned up some spaces - Minor docstring change because that's helpful

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import numpy
 import Tkinter
 import tkMessageBox
+import tkFileDialog
 
 from dynsound import DynSound
 
@@ -18,7 +19,7 @@ class App:
         CHANNELS (int): Number of channels.
         BUFFER (int): Size of buffer.
 
-        screen (pygame.Display): The main application screen.
+        main_screen (Tkinter.Tk): The main application screen.
         running (bool): If the program is running or not.
     """
 
@@ -30,6 +31,14 @@ class App:
     main_screen = None
     running = False
     edit_sound = None
+    base_sound = None  # Original loaded sound or sine wave
+    sound_needs_updating = True  # Whether parameters have changed and the sound needs to be regenerated
+
+    # Effects
+    volume = 0  # Volume offset
+    frequency = 1  # Frequency multiplier
+    frequency_change = 0  # in multiplier per second (TODO)
+    echo_count = 0  # Number of echoes
 
     def __init__(self):
         """Class constructor"""
@@ -37,20 +46,54 @@ class App:
         # Just call run and be done!
         self.run()
 
-    def helloCallBack(self):
-        tkMessageBox.showinfo("Hello Python", "Hello World")
+    def play_sound(self):
+        self.validate_sound()
+        self.edit_sound.play()
 
+    def save_sound(self):
+        self.validate_sound()
 
+        # Give the user a prompt to save the sound
+        filename = tkFileDialog.asksaveasfilename(initialdir="/", title="Save WAV as...", filetypes=[("Wave files", "*.wav")])
+
+        # Save it (unless the user cancelled)
+        if filename is not "":
+            self.edit_sound.save(filename)
+
+    def validate_sound(self):
+        """Regenerate sound from base elements"""
+        if not self.sound_needs_updating:
+            return
+
+        # Recopy the base sound
+        self.edit_sound = self.base_sound.copy()
+
+        # Apply effects in an order which hopefully minimises clipping: volume, frequency, echoes
+        if self.volume is not 0:
+            self.edit_sound.change_volume(self.volume)
+
+        if self.frequency is not 1.0:
+            self.edit_sound.change_frequency(self.frequency)
+
+        if self.echo_count > 0:
+            self.edit_sound.add_echo(0.3, -4, self.echo_count)
+
+        # Validate sound
+        self.sound_needs_updating = False
 
     def init_ui(self):
         """Initialise ui elements."""
 
-        play_preview = Tkinter.Button(self.main_screen, text="Play Sound", command=lambda: pygame.mixer.Sound.play(self.edit_sound.sound))
+        top_frame = Tkinter.Frame(self.main_screen)
+        play_preview = Tkinter.Button(top_frame, text="Play Sound", command=lambda: self.play_sound())
+        play_preview.pack(side=Tkinter.LEFT)
+        save_sound = Tkinter.Button(top_frame, text="Save Sound", command=lambda: self.save_sound())
+        save_sound.pack(side=Tkinter.LEFT)
 
-        volume_up = Tkinter.Button(self.main_screen, text="Volume Up", command=self.edit_sound.change_volume(10))
-        volume_down = Tkinter.Button(self.main_screen, text="Volume Down", command=self.edit_sound.change_volume(-10))
+        volume_up = Tkinter.Button(self.main_screen, text="Volume Up", command=lambda: self.change_volume(10))
+        volume_down = Tkinter.Button(self.main_screen, text="Volume Down", command=lambda: self.change_volume(-10))
 
-        play_preview.pack()
+        top_frame.pack()
         volume_up.pack()
         volume_down.pack()
 
@@ -63,7 +106,8 @@ class App:
 
         # self.screen = pygame.display.set_mode((640, 481))  # 481 for extra uniqueness points
 
-        self.edit_sound = self.create_sine(440, 1.0)
+        # Initialise a base sine wave
+        self.base_sound = self.create_sine(440, 1.0)
 
         self.main_screen = Tkinter.Tk()
         self.init_ui()
@@ -81,12 +125,19 @@ class App:
 
         self.edit_sound.save("testSound.wav")
 
-
-
     def quit(self):
         """Quits the application"""
 
         self.running = False
+
+    def change_volume(self, offset):
+        """Decreases the volume by offset
+
+        Args:
+            offset (float): The offset to increase (or decrease) volume by
+        """
+        self.volume += offset
+        self.sound_needs_updating = True
 
     def create_sine(self, frequency, length):
         """Creates a sine wave
@@ -106,6 +157,7 @@ class App:
         del samples
 
         return sound
+
 
 # Main code run!
 App()
