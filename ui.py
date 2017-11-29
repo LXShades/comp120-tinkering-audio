4cd9ea5dde31c6fb77b560833b31bfc7571e3d70
TMGilchirst <telcontar@hotmail.co.uk>
2017-11-29 15:40:58 +0000

    Updated laser_preset values. Added title label.

--- ui.py
+++ ui.py
@@ -47,6 +47,8 @@ class UI:
     button_parent = main_screen
     slider_parent = main_screen
 
+    title_text = None
+
     volume_slider = None
     frequency_slider = None
     frequency_shift_slider = None
@@ -69,50 +71,55 @@ class UI:
     def init_ui(self):
         """Initialise ui elements."""
 
+        # Title message.
+        self.title_text = Tkinter.Label(font=("Courier", 15), justify=Tkinter.CENTER, text="Sound Generation Tool by Louis Foy and Mango Gilchrist. \n Use the sliders below to choose the colour of your sound! \n"
+                                                                                           "Preset sounds are also available by clicking on the buttons below.")
+        self.title_text.grid(row=0, columnspan=4)
+
         # Preset sound buttons.
         self.death_preset_button = Tkinter.Button(self.button_parent, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, text="Death Sound", command=lambda: self.death_preset())
-        self.death_preset_button.grid(row=1, column=0)
+        self.death_preset_button.grid(row=2, column=0)
 
         self.jump_preset_button = Tkinter.Button(self.button_parent, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, text="Jump Sound", command=lambda: self.jump_preset())
-        self.jump_preset_button.grid(row=1, column=1)
+        self.jump_preset_button.grid(row=2, column=1)
 
         self.pickup_preset_button = Tkinter.Button(self.button_parent, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, text="Pickup Sound", command=lambda: self.pickup_preset())
-        self.pickup_preset_button.grid(row=1, column=2)
+        self.pickup_preset_button.grid(row=2, column=2)
 
         self.laser_preset_button = Tkinter.Button(self.button_parent, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, text="Laser Sound", command=lambda: self.laser_preset())
-        self.laser_preset_button.grid(row=1, column=3)
+        self.laser_preset_button.grid(row=2, column=3)
 
         # Play/Save buttons
         self.play_preview = Tkinter.Button(self.button_parent, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH * 2, text="Play Sound")
-        self.play_preview.grid(row=0, column=0, columnspan=2, sticky=Tkinter.W)
+        self.play_preview.grid(row=1, column=0, columnspan=2, sticky=Tkinter.W)
 
         self.save_sound = Tkinter.Button(self.button_parent, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH * 2, text="Save Sound")
-        self.save_sound.grid(row=0, column=2, columnspan=2, sticky=Tkinter.E)
+        self.save_sound.grid(row=1, column=2, columnspan=2, sticky=Tkinter.E)
 
         # Volume slider
         self.volume_slider = Tkinter.Scale(self.slider_parent, length=self.SLIDER_WIDTH, width=self.SLIDER_HEIGHT, troughcolor="#FFFF00", orient=Tkinter.HORIZONTAL, from_=0, to=100, showvalue=True)
         self.volume_slider.set(100)
-        self.volume_slider.grid(row=2, columnspan=4)
+        self.volume_slider.grid(row=3, columnspan=4)
 
         # Frequency slider
         self.frequency_slider = Tkinter.Scale(self.slider_parent, length=self.SLIDER_WIDTH, width=self.SLIDER_HEIGHT, troughcolor="#000000", orient=Tkinter.HORIZONTAL, from_=0.1, to=5.0, resolution=0.1, showvalue=True)
         self.frequency_slider.set(1)
-        self.frequency_slider.grid(row=3, columnspan=4)
+        self.frequency_slider.grid(row=4, columnspan=4)
 
         # Frequency shift slider
         self.frequency_shift_slider = Tkinter.Scale(self.slider_parent, length=self.SLIDER_WIDTH, width=self.SLIDER_HEIGHT, troughcolor="#ff0000", orient=Tkinter.HORIZONTAL, from_=0, to=5.0, resolution=0.1, showvalue=True)
         self.frequency_shift_slider.set(0)
-        self.frequency_shift_slider.grid(row=4, columnspan=4)
+        self.frequency_shift_slider.grid(row=5, columnspan=4)
 
         # Plop slider
         self.plop_slider = Tkinter.Scale(self.slider_parent, length=self.SLIDER_WIDTH, width=self.SLIDER_HEIGHT, troughcolor="#0000ff", orient=Tkinter.HORIZONTAL, from_=0, to=100, resolution=1, showvalue=True)
         self.plop_slider.set(1)
-        self.plop_slider.grid(row=5, columnspan=4)
+        self.plop_slider.grid(row=6, columnspan=4)
 
         # Echo slider
         self.echo_slider = Tkinter.Scale(self.slider_parent, length=self.SLIDER_WIDTH, width=self.SLIDER_HEIGHT, troughcolor="#800080", orient=Tkinter.HORIZONTAL, from_=0, to=10, showvalue=True)
         self.echo_slider.set(0)
-        self.echo_slider.grid(row=6, columnspan=4)
+        self.echo_slider.grid(row=7, columnspan=4)
 
     def death_preset(self):
         """Sets sliders to presets for death sound."""
@@ -141,10 +148,10 @@ class UI:
     def laser_preset(self):
         """Sets sliders to presets for laser sound."""
 
-        self.frequency_slider.set(2)
-        self.frequency_shift_slider.set(3)
+        self.frequency_slider.set(2.5)
+        self.frequency_shift_slider.set(2)
         self.plop_slider.set(80)
-        self.echo_slider.set(1)
+        self.echo_slider.set(0)
 
     def change_slider_colour(self, slider, (red, green, blue)):
         """Changes the colour of a slider by a red, green and blue value
